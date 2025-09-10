2b19b1c7112ccc47f7b68a6c0d1218d7e987418e
Lucas Fenstra <55227884+hogo1510@users.noreply.github.com>
2025-09-10 23:09:56 +0200

    base + pom

---
+++ src/main/java/org/lucas/brainrotmemeflashcards/BrainrotMemeFlashcardsApplication.java
@@ -0,0 +1,13 @@
+package org.lucas.brainrotmemeflashcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainrotMemeFlashcardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrainrotMemeFlashcardsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/lucas/brainrotmemeflashcards/BrainrotMemeFlashcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lucas.brainrotmemeflashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainrotMemeFlashcardsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
